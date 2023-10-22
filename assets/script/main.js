d1baee2854f2afd768ba74e60a857265fa3e9e05
Omonjon <omonjons077@gmail.com>
2023-10-22 16:40:33 +0500

    MORTGAGE section done 100%

--- assets/script/main.js
+++ assets/script/main.js
@@ -47,6 +47,32 @@ if ($(".photoSwiper").length) {
     });
 }
 
+/*=============== INPUT RANGE ===============*/
+const progress1 = document.querySelector('.progress-1');
+
+progress1.addEventListener('input', function () {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, var(--bs-secondary) 0%, var(--bs-secondary) ${value}%, #F8F8F8 ${value}%, #F8F8F8 100%)`
+})
+const progress2 = document.querySelector('.progress-2');
+
+progress2.addEventListener('input', function () {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, var(--bs-secondary) 0%, var(--bs-secondary) ${value}%, #F8F8F8 ${value}%, #F8F8F8 100%)`
+})
+const progress3 = document.querySelector('.progress-3');
+
+progress3.addEventListener('input', function () {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, var(--bs-secondary) 0%, var(--bs-secondary) ${value}%, #F8F8F8 ${value}%, #F8F8F8 100%)`
+})
+const progress4 = document.querySelector('.progress-4');
+
+progress4.addEventListener('input', function () {
+    const value = this.value;
+    this.style.background = `linear-gradient(to right, var(--bs-secondary) 0%, var(--bs-secondary) ${value}%, #F8F8F8 ${value}%, #F8F8F8 100%)`
+})
+
 /*=============== SHOW SCROLL UP ===============*/
 const scrollUp = () => {
     const scrollUp = document.getElementById('scroll-up')
